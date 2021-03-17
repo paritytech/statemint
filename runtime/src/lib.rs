322fdee28bf36ccbbbb928e41169d9f511f8fd99
JesseAbram <jesseabramowitz@hotmail.com>
2021-03-17 13:52:35 +0100

    change node template to statemint

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -93,8 +93,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("statemint"),
+	impl_name: create_runtime_str!("statemint"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
