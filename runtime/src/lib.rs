c6e733a3248f24a266775431e9c51a73fa4ac0ce
joepetrowski <joe@parity.io>
2021-03-23 17:44:45 +0100

    add asset proxy types

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -271,6 +271,8 @@ impl pallet_assets::Config for Runtime {
 	type Balance = u64;
 	type AssetId = u32;
 	type Currency = Balances;
+	// TODO: Change to proportion at least 60% (3/5) of Relay Chain Council.
+	// https://github.com/paritytech/statemint/issues/4
 	type ForceOrigin = frame_system::EnsureRoot<AccountId>;
 	type AssetDeposit = AssetDeposit;
 	type MetadataDepositBase = MetadataDepositBase;
@@ -320,6 +322,11 @@ parameter_types! {
 pub enum ProxyType {
 	Any,
 	NonTransfer,
+	CancelProxy,
+	Assets,
+	AssetOwner,
+	AssetManager,
+	// Staking, // TODO: Add when complete: https://github.com/paritytech/statemint/issues/9
 }
 impl Default for ProxyType { fn default() -> Self { Self::Any } }
 impl InstanceFilter<Call> for ProxyType {
@@ -328,8 +335,43 @@ impl InstanceFilter<Call> for ProxyType {
 			ProxyType::Any => true,
 			ProxyType::NonTransfer => !matches!(
 				c,
-				Call::Balances(..)
-			)
+				Call::Balances(..) |
+				Call::Assets(pallet_assets::Call::transfer(..)) |
+				Call::Assets(pallet_assets::Call::transfer_keep_alive(..)) |
+				Call::Assets(pallet_assets::Call::force_transfer(..)) |
+				Call::Assets(pallet_assets::Call::approve_transfer(..)) |
+				Call::Assets(pallet_assets::Call::transfer_approved(..))
+			),
+			ProxyType::CancelProxy => matches!(c,
+				Call::Proxy(pallet_proxy::Call::reject_announcement(..)) |
+				Call::Utility(..) |
+				Call::Multisig(..)
+			),
+			ProxyType::Assets => matches!(c,
+				Call::Assets(..) |
+				Call::Utility(..) |
+				Call::Multisig(..)
+			),
+			ProxyType::AssetOwner => matches!(c,
+				Call::Assets(pallet_assets::Call::create(..)) |
+				Call::Assets(pallet_assets::Call::destroy(..)) |
+				Call::Assets(pallet_assets::Call::transfer_ownership(..)) |
+				Call::Assets(pallet_assets::Call::set_team(..)) |
+				Call::Assets(pallet_assets::Call::set_metadata(..)) |
+				Call::Assets(pallet_assets::Call::clear_metadata(..)) |
+				Call::Utility(..) |
+				Call::Multisig(..)
+			),
+			ProxyType::AssetManager => matches!(c,
+				Call::Assets(pallet_assets::Call::mint(..)) |
+				Call::Assets(pallet_assets::Call::burn(..)) |
+				Call::Assets(pallet_assets::Call::freeze(..)) |
+				Call::Assets(pallet_assets::Call::thaw(..)) |
+				Call::Assets(pallet_assets::Call::freeze_asset(..)) |
+				Call::Assets(pallet_assets::Call::thaw_asset(..)) |
+				Call::Utility(..) |
+				Call::Multisig(..)
+			),
 		}
 	}
 	fn is_superset(&self, o: &Self) -> bool {
@@ -337,7 +379,7 @@ impl InstanceFilter<Call> for ProxyType {
 			(x, y) if x == y => true,
 			(ProxyType::Any, _) => true,
 			(_, ProxyType::Any) => false,
-			(ProxyType::NonTransfer, _) => true,
+			_ => false,
 		}
 	}
 }
