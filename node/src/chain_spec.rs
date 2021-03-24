824f3516d908b054dd8e43de05281a2574730bf6
Ricardo Rius <ricardo@parity.io>
2021-03-24 12:13:26 +0100

    Fix format

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,10 +1,10 @@
 use cumulus_primitives_core::ParaId;
-use statemint_runtime::{AccountId, Signature};
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
 use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use statemint_runtime::{AccountId, Signature};
 
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec = sc_service::GenericChainSpec<statemint_runtime::GenesisConfig, Extensions>;
--- node/src/cli.rs
+++ node/src/cli.rs
@@ -6,88 +6,88 @@ use structopt::StructOpt;
 /// Sub-commands supported by the collator.
 #[derive(Debug, StructOpt)]
 pub enum Subcommand {
-	/// Export the genesis state of the parachain.
-	#[structopt(name = "export-genesis-state")]
-	ExportGenesisState(ExportGenesisStateCommand),
+    /// Export the genesis state of the parachain.
+    #[structopt(name = "export-genesis-state")]
+    ExportGenesisState(ExportGenesisStateCommand),
 
-	/// Export the genesis wasm of the parachain.
-	#[structopt(name = "export-genesis-wasm")]
-	ExportGenesisWasm(ExportGenesisWasmCommand),
+    /// Export the genesis wasm of the parachain.
+    #[structopt(name = "export-genesis-wasm")]
+    ExportGenesisWasm(ExportGenesisWasmCommand),
 
-	/// Build a chain specification.
-	BuildSpec(sc_cli::BuildSpecCmd),
+    /// Build a chain specification.
+    BuildSpec(sc_cli::BuildSpecCmd),
 
-	/// Validate blocks.
-	CheckBlock(sc_cli::CheckBlockCmd),
+    /// Validate blocks.
+    CheckBlock(sc_cli::CheckBlockCmd),
 
-	/// Export blocks.
-	ExportBlocks(sc_cli::ExportBlocksCmd),
+    /// Export blocks.
+    ExportBlocks(sc_cli::ExportBlocksCmd),
 
-	/// Export the state of a given block into a chain spec.
-	ExportState(sc_cli::ExportStateCmd),
+    /// Export the state of a given block into a chain spec.
+    ExportState(sc_cli::ExportStateCmd),
 
-	/// Import blocks.
-	ImportBlocks(sc_cli::ImportBlocksCmd),
+    /// Import blocks.
+    ImportBlocks(sc_cli::ImportBlocksCmd),
 
-	/// Remove the whole chain.
-	PurgeChain(sc_cli::PurgeChainCmd),
+    /// Remove the whole chain.
+    PurgeChain(sc_cli::PurgeChainCmd),
 
-	/// Revert the chain to a previous state.
-	Revert(sc_cli::RevertCmd),
+    /// Revert the chain to a previous state.
+    Revert(sc_cli::RevertCmd),
 }
 
 /// Command for exporting the genesis state of the parachain
 #[derive(Debug, StructOpt)]
 pub struct ExportGenesisStateCommand {
-	/// Output file name or stdout if unspecified.
-	#[structopt(parse(from_os_str))]
-	pub output: Option<PathBuf>,
+    /// Output file name or stdout if unspecified.
+    #[structopt(parse(from_os_str))]
+    pub output: Option<PathBuf>,
 
-	/// Id of the parachain this state is for.
-	#[structopt(long, default_value = "200")]
-	pub parachain_id: u32,
+    /// Id of the parachain this state is for.
+    #[structopt(long, default_value = "200")]
+    pub parachain_id: u32,
 
-	/// Write output in binary. Default is to write in hex.
-	#[structopt(short, long)]
-	pub raw: bool,
+    /// Write output in binary. Default is to write in hex.
+    #[structopt(short, long)]
+    pub raw: bool,
 
-	/// The name of the chain for that the genesis state should be exported.
-	#[structopt(long)]
-	pub chain: Option<String>,
+    /// The name of the chain for that the genesis state should be exported.
+    #[structopt(long)]
+    pub chain: Option<String>,
 }
 
 /// Command for exporting the genesis wasm file.
 #[derive(Debug, StructOpt)]
 pub struct ExportGenesisWasmCommand {
-	/// Output file name or stdout if unspecified.
-	#[structopt(parse(from_os_str))]
-	pub output: Option<PathBuf>,
+    /// Output file name or stdout if unspecified.
+    #[structopt(parse(from_os_str))]
+    pub output: Option<PathBuf>,
 
-	/// Write output in binary. Default is to write in hex.
-	#[structopt(short, long)]
-	pub raw: bool,
+    /// Write output in binary. Default is to write in hex.
+    #[structopt(short, long)]
+    pub raw: bool,
 
-	/// The name of the chain for that the genesis wasm file should be exported.
-	#[structopt(long)]
-	pub chain: Option<String>,
+    /// The name of the chain for that the genesis wasm file should be exported.
+    #[structopt(long)]
+    pub chain: Option<String>,
 }
 
 #[derive(Debug, StructOpt)]
 pub struct RunCmd {
-	#[structopt(flatten)]
-	pub base: sc_cli::RunCmd,
+    #[structopt(flatten)]
+    pub base: sc_cli::RunCmd,
 
-	/// Id of the parachain this collator collates for.
-	#[structopt(long)]
-	pub parachain_id: Option<u32>,
+    /// Id of the parachain this collator collates for.
+    #[structopt(long)]
+    pub parachain_id: Option<u32>,
 }
 
 impl std::ops::Deref for RunCmd {
-	type Target = sc_cli::RunCmd;
+    type Target = sc_cli::RunCmd;
 
-	fn deref(&self) -> &Self::Target {
-		&self.base
-	}
+    fn deref(&self) -> &Self::Target {
+        &self.base
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -97,46 +97,46 @@ impl std::ops::Deref for RunCmd {
 	structopt::clap::AppSettings::SubcommandsNegateReqs,
 ])]
 pub struct Cli {
-	#[structopt(subcommand)]
-	pub subcommand: Option<Subcommand>,
+    #[structopt(subcommand)]
+    pub subcommand: Option<Subcommand>,
 
-	#[structopt(flatten)]
-	pub run: RunCmd,
+    #[structopt(flatten)]
+    pub run: RunCmd,
 
-	/// Run node as collator.
-	///
-	/// Note that this is the same as running with `--validator`.
-	#[structopt(long, conflicts_with = "validator")]
-	pub collator: bool,
+    /// Run node as collator.
+    ///
+    /// Note that this is the same as running with `--validator`.
+    #[structopt(long, conflicts_with = "validator")]
+    pub collator: bool,
 
-	/// Relaychain arguments
-	#[structopt(raw = true)]
-	pub relaychain_args: Vec<String>,
+    /// Relaychain arguments
+    #[structopt(raw = true)]
+    pub relaychain_args: Vec<String>,
 }
 
 #[derive(Debug)]
 pub struct RelayChainCli {
-	/// The actual relay chain cli object.
-	pub base: polkadot_cli::RunCmd,
+    /// The actual relay chain cli object.
+    pub base: polkadot_cli::RunCmd,
 
-	/// Optional chain id that should be passed to the relay chain.
-	pub chain_id: Option<String>,
+    /// Optional chain id that should be passed to the relay chain.
+    pub chain_id: Option<String>,
 
-	/// The base path that should be used by the relay chain.
-	pub base_path: Option<PathBuf>,
+    /// The base path that should be used by the relay chain.
+    pub base_path: Option<PathBuf>,
 }
 
 impl RelayChainCli {
-	/// Create a new instance of `Self`.
-	pub fn new<'a>(
-		base_path: Option<PathBuf>,
-		chain_id: Option<String>,
-		relay_chain_args: impl Iterator<Item = &'a String>,
-	) -> Self {
-		Self {
-			base_path,
-			chain_id,
-			base: polkadot_cli::RunCmd::from_iter(relay_chain_args),
-		}
-	}
+    /// Create a new instance of `Self`.
+    pub fn new<'a>(
+        base_path: Option<PathBuf>,
+        chain_id: Option<String>,
+        relay_chain_args: impl Iterator<Item = &'a String>,
+    ) -> Self {
+        Self {
+            base_path,
+            chain_id,
+            base: polkadot_cli::RunCmd::from_iter(relay_chain_args),
+        }
+    }
 }
