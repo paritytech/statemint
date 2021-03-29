1aa78d72443296ed511de844be21e3bc0bfc57e5
Ricardo Rius <ricardo@parity.io>
2021-03-29 13:26:14 +0200

    Fix fmt2

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -11,26 +11,26 @@ pub type ChainSpec = sc_service::GenericChainSpec<statemint_runtime::GenesisConf
 
 /// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-		TPublic::Pair::from_string(&format!("//{}", seed), None)
-				.expect("static values are valid; qed")
-				.public()
+	TPublic::Pair::from_string(&format!("//{}", seed), None)
+		.expect("static values are valid; qed")
+		.public()
 }
 
 /// The extensions for the [`ChainSpec`].
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ChainSpecGroup, ChainSpecExtension)]
 #[serde(deny_unknown_fields)]
 pub struct Extensions {
-		/// The relay chain of the Parachain.
-		pub relay_chain: String,
-		/// The id of the Parachain.
-		pub para_id: u32,
+	/// The relay chain of the Parachain.
+	pub relay_chain: String,
+	/// The id of the Parachain.
+	pub para_id: u32,
 }
 
 impl Extensions {
-		/// Try to get the extension from the given `ChainSpec`.
-		pub fn try_get(chain_spec: &dyn sc_service::ChainSpec) -> Option<&Self> {
-				sc_chain_spec::get_extension(chain_spec.extensions())
-		}
+	/// Try to get the extension from the given `ChainSpec`.
+	pub fn try_get(chain_spec: &dyn sc_service::ChainSpec) -> Option<&Self> {
+		sc_chain_spec::get_extension(chain_spec.extensions())
+	}
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
@@ -38,99 +38,99 @@ type AccountPublic = <Signature as Verify>::Signer;
 /// Helper function to generate an account ID from seed
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
 where
-		AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
 {
-		AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 pub fn development_config(id: ParaId) -> ChainSpec {
-		ChainSpec::from_genesis(
-				// Name
-				"Development",
-				// ID
-				"dev",
-				ChainType::Local,
-				move || {
-						testnet_genesis(
-								get_account_id_from_seed::<sr25519::Public>("Alice"),
-								vec![
-										get_account_id_from_seed::<sr25519::Public>("Alice"),
-										get_account_id_from_seed::<sr25519::Public>("Bob"),
-										get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-								],
-								id,
-						)
-				},
-				vec![],
-				None,
-				None,
-				None,
-				Extensions {
-						relay_chain: "rococo-dev".into(),
-						para_id: id.into(),
-				},
-		)
+	ChainSpec::from_genesis(
+		// Name
+		"Development",
+		// ID
+		"dev",
+		ChainType::Local,
+		move || {
+			testnet_genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				],
+				id,
+			)
+		},
+		vec![],
+		None,
+		None,
+		None,
+		Extensions {
+			relay_chain: "rococo-dev".into(),
+			para_id: id.into(),
+		},
+	)
 }
 
 pub fn local_testnet_config(id: ParaId) -> ChainSpec {
-		ChainSpec::from_genesis(
-				// Name
-				"Local Testnet",
-				// ID
-				"local_testnet",
-				ChainType::Local,
-				move || {
-						testnet_genesis(
-								get_account_id_from_seed::<sr25519::Public>("Alice"),
-								vec![
-										get_account_id_from_seed::<sr25519::Public>("Alice"),
-										get_account_id_from_seed::<sr25519::Public>("Bob"),
-										get_account_id_from_seed::<sr25519::Public>("Charlie"),
-										get_account_id_from_seed::<sr25519::Public>("Dave"),
-										get_account_id_from_seed::<sr25519::Public>("Eve"),
-										get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-										get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-										get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-								],
-								id,
-						)
-				},
-				vec![],
-				None,
-				None,
-				None,
-				Extensions {
-						relay_chain: "rococo-local".into(),
-						para_id: id.into(),
-				},
-		)
+	ChainSpec::from_genesis(
+		// Name
+		"Local Testnet",
+		// ID
+		"local_testnet",
+		ChainType::Local,
+		move || {
+			testnet_genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+				],
+				id,
+			)
+		},
+		vec![],
+		None,
+		None,
+		None,
+		Extensions {
+			relay_chain: "rococo-local".into(),
+			para_id: id.into(),
+		},
+	)
 }
 
 fn testnet_genesis(
-		root_key: AccountId,
-		endowed_accounts: Vec<AccountId>,
-		id: ParaId,
+	root_key: AccountId,
+	endowed_accounts: Vec<AccountId>,
+	id: ParaId,
 ) -> statemint_runtime::GenesisConfig {
-		statemint_runtime::GenesisConfig {
-				frame_system: statemint_runtime::SystemConfig {
-						code: statemint_runtime::WASM_BINARY
-								.expect("WASM binary was not build, please build it!")
-								.to_vec(),
-						changes_trie_config: Default::default(),
-				},
-				pallet_balances: statemint_runtime::BalancesConfig {
-						balances: endowed_accounts
-								.iter()
-								.cloned()
-								.map(|k| (k, 1 << 60))
-								.collect(),
-				},
-				pallet_sudo: statemint_runtime::SudoConfig { key: root_key },
-				parachain_info: statemint_runtime::ParachainInfoConfig { parachain_id: id },
-		}
+	statemint_runtime::GenesisConfig {
+		frame_system: statemint_runtime::SystemConfig {
+			code: statemint_runtime::WASM_BINARY
+				.expect("WASM binary was not build, please build it!")
+				.to_vec(),
+			changes_trie_config: Default::default(),
+		},
+		pallet_balances: statemint_runtime::BalancesConfig {
+			balances: endowed_accounts
+				.iter()
+				.cloned()
+				.map(|k| (k, 1 << 60))
+				.collect(),
+		},
+		pallet_sudo: statemint_runtime::SudoConfig { key: root_key },
+		parachain_info: statemint_runtime::ParachainInfoConfig { parachain_id: id },
+	}
 }
