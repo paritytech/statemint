564273dfda1aca4fb9618fd7e40958e4926e0c4e
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2021-03-31 16:51:07 +0200

    Split Statemine and Statemint (#24)
    * split
    
    * added statemine to chainspec
    
    * tweak statemine params
    
    * remove comment
    
    Co-authored-by: joepetrowski <joe@parity.io>

--- runtime/statemint/src/lib.rs
+++ runtime/statemint/src/lib.rs
@@ -7,7 +7,6 @@
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 pub mod constants;
-pub mod impls;
 
 use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
@@ -58,7 +57,7 @@ pub use frame_support::{
 	},
 	RuntimeDebug, StorageValue,
 };
-use impls::DealWithFees;
+use runtime_common::impls::DealWithFees;
 pub use pallet_balances::Call as BalancesCall;
 pub use pallet_timestamp::Call as TimestampCall;
 #[cfg(any(feature = "std", test))]
@@ -138,9 +137,6 @@ pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
 pub const HOURS: BlockNumber = MINUTES * 60;
 pub const DAYS: BlockNumber = HOURS * 24;
-
-// 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
-pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
 
 #[derive(codec::Encode, codec::Decode)]
 pub enum XCMPMessage<XAccountId, XBalance> {
