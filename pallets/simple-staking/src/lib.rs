7332288684989ee342f391280707d4f05662179a
JesseAbram <jesseabramowitz@hotmail.com>
2021-04-06 17:08:18 +0200

    added try mutate

--- pallets/simple-staking/src/lib.rs
+++ pallets/simple-staking/src/lib.rs
@@ -17,7 +17,7 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*, traits::{Currency, Vec}};
-	use frame_system::pallet_prelude::*;
+	use frame_system::{pallet_prelude::*, ensure_root};
 	use frame_system::Config as SystemConfig;
 	use frame_support::sp_runtime::{
 		RuntimeDebug,
@@ -36,9 +36,10 @@ pub mod pallet {
 
 
 	#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
-	pub struct AuthorInfo<AccountId, Balance> {
+	pub struct AuthorInfo<AccountId, Balance, BlockNumber> {
 		who: AccountId,
-		deposit: Balance
+		deposit: Balance,
+		last_block: BlockNumber
 	}
 
 	#[pallet::pallet]
@@ -54,7 +55,7 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn authors)]
-	pub type Authors<T: Config> = StorageValue<_, Vec<AuthorInfo<T::AccountId, T::Balance>>>;
+	pub type Authors<T: Config> = StorageValue<_, Vec<AuthorInfo<T::AccountId, T::Balance, T::BlockNumber>>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn max_authors)]
@@ -71,16 +72,18 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		NewInvulnerables(Vec<T::AccountId>),
+		NewMaxAuthorCount(u32),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		MaxAuthors,
+		Unknown,
+		Permission,
+		AlreadyAuthor,
+		NotAuthor
 	}
 
 	#[pallet::hooks]
@@ -97,33 +100,56 @@ pub mod pallet {
 
 
 		#[pallet::weight(10_000)]
-		fn set_invulnerables(origin: OriginFor<T>, new_invulnerables: Vec<T::AccountId>) -> DispatchResultWithPostInfo {
-			// Protect function 
-			// sets invulnerables
+		fn set_invulnerables(origin: OriginFor<T>, new: Vec<T::AccountId>) -> DispatchResultWithPostInfo {
+			//TODO chose protective scheme
+			ensure_root(origin)?;
+			<Invulnerables<T>>::put(&new);
+			Self::deposit_event(Event::NewInvulnerables(new));
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		fn set_max_author_count(origin: OriginFor<T>, max_authors: u32) -> DispatchResultWithPostInfo {
+			//TODO chose protective scheme
+			ensure_root(origin)?;
+			<MaxAuthors<T>>::put(&max_authors);
+			Self::deposit_event(Event::NewMaxAuthorCount(max_authors));
+			// sets max authors
 			Ok(().into())
 
 		}
 
 		#[pallet::weight(10_000)]
-		fn set_max_author_count(origin: OriginFor<T>, max_authors: u64) -> DispatchResultWithPostInfo {
-			// Protect function 
-			// sets invulnerables
+		fn author_intent(origin: OriginFor<T>, deposit: T::Balance) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			ensure!(<Authors<T>>::decode_len().ok_or(Error::<T>::Unknown)? + 1 <= MaxAuthors::<T>::get().ok_or(Error::<T>::Unknown)? as usize, Error::<T>::MaxAuthors); 
+			let new_author = AuthorInfo {
+				who: who.clone(), 
+				deposit, 
+				last_block: frame_system::Pallet::<T>::block_number()
+
+			};
+			//take deposit
+			<Authors<T>>::try_mutate(|authors| -> DispatchResult {
+				let exists = authors.into_iter().any(|author| author.who == who);
+				match exists {
+					true => Ok(authors.push(new_author)),
+					false => Err(Error::<T>::AlreadyAuthor)?,
+				}
+			})?;
 			Ok(().into())
-
-		}
-
-		#[pallet::weight(10_000)]
-		fn author_intent(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-			// no stake for now?
-			// checks to make sure not max
-			// adds them to list
-			Ok(().into())
-
 		}
 
 		#[pallet::weight(10_000)]
 		fn leave_intent(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-			// removes them to list
+			let who = ensure_signed(origin)?;
+			<Authors<T>>::try_mutate(|authors| -> DispatchResult {
+				let index = authors.iter().position(|author| author.who == who).ok_or(Error::<T>::NotAuthor)?;
+				authors.remove(index);
+				Ok(())
+			})?;
+
+
 			Ok(().into())
 
 		}
