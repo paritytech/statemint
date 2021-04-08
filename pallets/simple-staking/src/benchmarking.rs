c9cbe28749507de77dff83021fab49f07b27be11
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2021-04-08 09:52:12 +0200

    Update pallets/simple-staking/src/benchmarking.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- pallets/simple-staking/src/benchmarking.rs
+++ pallets/simple-staking/src/benchmarking.rs
@@ -17,4 +17,4 @@ benchmarks! {
 	}
 }
 
-impl_benchmark_test_suite!(simple_staking, crate::mock::new_test_ext(), crate::mock::Test,);
+impl_benchmark_test_suite!(SimpleStaking, crate::mock::new_test_ext(), crate::mock::Test,);
